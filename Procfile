3d08ee62dc5b885af8ba13250a284cebf8d26e14
Jannat <131643152+jannat93@users.noreply.github.com>
2025-12-07 01:46:33 -0300

    Fixed nested repo

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn expense_api.wsgi
--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 from .views import ExpenseViewSet
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'expenses', ExpenseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
